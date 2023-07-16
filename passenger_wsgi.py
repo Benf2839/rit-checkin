9741b5c8a0e8c28a745b1f2b41fbd164543c19ef
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-07-15 20:28:06 -0400

    updates for server testing

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,14 +1,3 @@
-import sys
-import os
+#import wsgi.py file from web_projects sub folder within destiny-project folder
+from web_project.wsgi import application
 
-# Add your project's virtual environment path
-# Replace '/path/to/venv' with the actual path
-project_base = '.venv'
-python_version = 'python3.11.2'
-
-sys.path.append(project_base)
-sys.path.append(os.getcwd())
-sys.executable = os.path.join(project_base, 'bin', python_version)
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()

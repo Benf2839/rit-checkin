f3639ce68119b1bae08c2049f3092c31d7196077
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-07-18 14:05:44 -0400

    testing email send aliasing

--- hello/views.py
+++ hello/views.py
@@ -271,21 +271,21 @@ def export_data(request):
 
 
 def send_reset_email(request):  
-   if request.method == "POST": 
-       with get_connection(  
-           host=settings.EMAIL_HOST, 
-     port=settings.EMAIL_PORT,  
-     username=settings.EMAIL_HOST_USER, 
-     password=settings.EMAIL_HOST_PASSWORD, 
-     use_tls=settings.EMAIL_USE_TLS  
-       ) as connection:  
+    if request.method == "POST": 
+        with get_connection(  
+            host=settings.EMAIL_HOST, 
+            port=settings.EMAIL_PORT,  
+            username=settings.EMAIL_HOST_USER, 
+            password=settings.EMAIL_HOST_PASSWORD, 
+            use_tls=settings.EMAIL_USE_TLS  
+        ) as connection:  
            subject = request.POST.get("Password Reset")  
            email_from = settings.DEFAULT_FROM_EMAIL  
            recipient_list = [request.POST.get("email"), ]  
            message = request.POST.get("message")  
            EmailMessage(subject, message, email_from, recipient_list, connection=connection).send()  
  
-   return render(request, 'password_reset_email.html')
+    return render(request, 'password_reset_email.html')
 
 
 def send_qr_email(request):

053e7a2c16f56f6c0d538d0f17c1aaf7f6317155
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-08-08 14:03:37 -0400

    updating add entry to account for new field

--- hello/views.py
+++ hello/views.py
@@ -444,6 +444,7 @@ def add_entry(request):
             checked_in=True,
             checked_in_time=timezone.now(),  # Autopopulate with current time and date,
             table_number=table_number
+            email_sent=False
         )
 
 

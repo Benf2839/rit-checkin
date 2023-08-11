8e07ba0a227823d934291da6b7e6880bb88d2f27
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-08-11 12:12:15 -0400

    modifying timezone settings

--- hello/production_settings.py
+++ hello/production_settings.py
@@ -106,7 +106,7 @@ DATABASES = {
 'HOST': 'localhost', # use localhost for on server testing and 198.38.88.120 for computer testing
 'PORT': '3306',
 'OPTIONS': {
-            'init_command': "SET time_zone='America/New_York';",
+            'init_command': "SET time_zone='-04:00';",
         },
 }
 }

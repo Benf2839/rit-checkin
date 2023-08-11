7b907ea845a90eaa43ace16d64c9bac74f705a70
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-08-11 11:37:19 -0400

    changing function

--- hello/views.py
+++ hello/views.py
@@ -56,6 +56,7 @@ def load_add_new_data_page(request):
 
 def redirect_w_backup(request):
     backup_file_path = '/home/guardia2/Web_app/Exports/db_backup.csv'
+    
     if os.path.exists(backup_file_path):
         try:
             return FileResponse(open(backup_file_path, 'rb'), as_attachment=True, filename='server_backup.csv')
@@ -63,8 +64,8 @@ def redirect_w_backup(request):
             error_message = f'Error occurred while processing the backup: {str(e)}'
             return HttpResponseServerError(error_message)
     else:
-        messages.error(request, 'No Backup found.')  # Display an error message
-        return render(request, 'hello/database_upload_page.html')
+        error_message = 'No backup found.'
+        return HttpResponseServerError(error_message)
     
 
 def export_master_list(request):

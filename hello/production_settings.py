d9664d873cc10070a450afda60e2345c8c3841e4
Ben Fauteux <bdf2768@rit.edu>
2023-09-05 13:28:20 -0400

    re adding secret key

--- hello/production_settings.py
+++ hello/production_settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from django.utils import timezone
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

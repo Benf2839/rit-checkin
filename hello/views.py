6d62bd9475797fe581680f250dcbd8179a7cc003
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-09-11 11:27:02 -0400

    fixing table num issue

--- hello/views.py
+++ hello/views.py
@@ -632,7 +632,7 @@ def db_display(request, page=1):
         # Convert 'checked_in' field
         converted_row[7] = 'yes' if converted_row[7] == 1 else 'no'
         # Convert 'email_sent' field
-        converted_row[9] = 'yes' if converted_row[9] == 1 else 'no'
+        converted_row[10] = 'yes' if converted_row[10] == 1 else 'no'
         converted_rows.append(converted_row)
 
 

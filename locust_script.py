b7fd2569c57fe97c93c14da7cf00bb21ac387212
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-09-18 09:18:22 -0400

    updating locust settings

--- locust_script.py
+++ locust_script.py
@@ -1,10 +1,21 @@
 from locust import HttpUser, task, between
 import random
 import string
+import re  # For regular expressions
+
+# use  locust -f locust_script.py  to run the testing
 
 
 class MyUser(HttpUser):
-    wait_time = between(1, 5)  # Wait between 1 and 5 seconds between tasks
+    wait_time = between(10, 20)  # Wait between 1 and 5 seconds between tasks
+
+    def on_start(self):
+        # Perform a GET request to the page to obtain the CSRF token
+        response = self.client.get(
+            "https://eventcheck-in.com/self_registration/")
+        csrf_token = self.extract_csrf_token(
+            response.text)  # Extract the CSRF token
+        self.csrf_token = csrf_token
 
     @task
     def enter_info_and_submit(self):
@@ -13,8 +24,9 @@ class MyUser(HttpUser):
         last_name = ''.join(random.choices(string.ascii_letters, k=8))
         email = f"{first_name.lower()}_{last_name.lower()}@example.com"
 
-        # Define the data to be submitted in the POST request
+        # Define the data to be submitted in the POST request, including the CSRF token
         data = {
+            'csrfmiddlewaretoken': self.csrf_token,  # Include the CSRF token here
             'first_name': first_name,
             'last_name': last_name,
             'email': email,
@@ -24,4 +36,14 @@ class MyUser(HttpUser):
         }
 
         # Send a POST request to submit the form
-        self.client.post("https://eventcheck-in.com/add_entry/", data=data)
+        self.client.post(
+            "https://eventcheck-in.com/self_registration/", data=data)
+
+    def extract_csrf_token(self, response_text):
+        # Use regular expressions to extract the CSRF token from the HTML response
+        csrf_token_match = re.search(
+            r'name="csrfmiddlewaretoken" value="(.+?)"', response_text)
+        if csrf_token_match:
+            return csrf_token_match.group(1)
+        else:
+            raise ValueError("CSRF token not found in HTML response")

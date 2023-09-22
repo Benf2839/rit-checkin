1479b1d754034ba07a18ff28aa81c3d0ddd3b60c
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-09-22 12:34:49 -0400

    updating source

--- send_emails.sh
+++ send_emails.sh
@@ -4,7 +4,7 @@
 cd /home/guardia2/Web_apps
 
 # Activate your virtual environment if needed
- source /home/guardia2/virtualenv/Web_apps/3.10/bin/activate.ps1
+source /home/guardia2/virtualenv/Web_apps/3.10/bin/activate && cd /home/guardia2/Web_apps
 
 # Run the send_emails management command
 python manage.py send_emails

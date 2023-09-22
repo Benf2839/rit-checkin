b667f950104be2a012751df2205d59abde673533
Benf2839 <104294907+Benf2839@users.noreply.github.com>
2023-09-22 12:30:08 -0400

    updating path to venv .ps1 for cron

--- send_emails.sh
+++ send_emails.sh
@@ -4,7 +4,7 @@
 cd /home/guardia2/Web_apps
 
 # Activate your virtual environment if needed
- #source /home/guardia2/Web_apps/venv/bin/activate
+ source /home/guardia2/virtualenv/Web_apps/3.10/bin/activate.ps1
 
 # Run the send_emails management command
 python manage.py send_emails

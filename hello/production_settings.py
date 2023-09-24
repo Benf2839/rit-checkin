96bd0c5c1209a20a323aca5db8d5b6313b34704f
Ben Fauteux <bdf2768@rit.edu>
2023-09-24 16:58:02 -0400

    fixing email backend issue

--- hello/production_settings.py
+++ hello/production_settings.py
@@ -170,8 +170,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_STARTTLS = True
 DEFAULT_FROM_EMAIL = 'ritcareerfair@eventcheck-in.com'
 SERVER_EMAIL = EMAIL_HOST_USER
-EMAIL_BACKEND = 'email_tracker.backends.EmailTrackerBackend'
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # WALLETPASS = {
 #    'CERT_PATH': '',

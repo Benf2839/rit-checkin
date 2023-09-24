98eafcc4639d8cb957f9e9b1532d3d11e317ad9f
Ben Fauteux <bdf2768@rit.edu>
2023-09-24 17:21:23 -0400

    updating functions.py for email sending

--- hello/functions.py
+++ hello/functions.py
@@ -1,18 +1,18 @@
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
-from hello.models import db_model, EmailConfiguration  # Import your models
+from hello.models import db_model  # Replace with your actual model
 
 def send_emails_in_batches(batch_size):
     try:
-        # Check if auto email sending is enabled
-        email_config = EmailConfiguration.objects.filter(pk=1).first()
-        if email_config and email_config.auto_email_sending_active == 0:
-            return [], ["Auto email sending is disabled, so no emails were sent."]
-
         # Initialize lists to store successful and failed email addresses
         successful_emails = []
         failed_emails = []
+
+        # Check if auto email sending is active
+        config = EmailConfiguration.objects.filter(id=1).first()
+        if not config or not config.auto_email_sending_active:
+            return successful_emails, ["Auto email sending is disabled, no emails were sent."]
 
         # Retrieve all records where email_sent is False and limit to batch_size
         records = db_model.objects.filter(email_sent=False)[:batch_size]
@@ -40,10 +40,9 @@ def send_emails_in_batches(batch_size):
                 record.save()
                 successful_emails.append(record.email)
             except Exception as e:
-                # Append the failed email and exception details to the failed_emails list
-                failed_emails.append({'email': record.email, 'error': str(e)})
+                failed_emails.append(f"Email: {record.email}, Error: {str(e)}")
 
         return successful_emails, failed_emails
 
     except Exception as e:
-        return [], [{'error': str(e)}]
+        return [], [str(e)]

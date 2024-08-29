8619588598204051dd984f3b04e33f75f5fa3eb5
Benjamin Fauteux <104294907+Benf2839@users.noreply.github.com>
2024-08-29 13:44:13 -0400

    Switching to localhost test setup

--- hello/test_settings.py
+++ hello/test_settings.py
@@ -146,7 +146,7 @@ DATABASES = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "test.db",
     }
 }
 
